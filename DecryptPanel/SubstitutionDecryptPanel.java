445981280608d4df45f72ca5d0d31bed196332cc
WinterUnity <82164361+WinterUnity@users.noreply.github.com>
2022-11-12 09:26:45 +0700

    Add files via upload

---
+++ DecryptPanel/SubstitutionDecryptPanel.java
@@ -0,0 +1,104 @@
+package DecryptPanel;
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
+
+public class SubstitutionDecryptPanel extends JPanel {
+	JPanel top, mid, bot, subPanel;
+
+	public SubstitutionDecryptPanel() {
+		setLayout(new BorderLayout());
+
+		/*
+		 * Top Layout
+		 */
+		top = new JPanel();
+		top.setLayout(new GridLayout(5,1));
+		Border topBD = BorderFactory.createLineBorder(Color.blue);
+		top.setBorder(BorderFactory.createTitledBorder(topBD, "Thông tin - Giải mã Substitution"));
+
+		// CipherText
+		JPanel ctPanel = new JPanel();
+		ctPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+
+		JLabel ptLabel = new JLabel("Cipher Text");
+		JTextField ptTextField = new JTextField(30);
+		ptTextField.setHorizontalAlignment(SwingConstants.RIGHT);
+		ctPanel.add(ptLabel);
+		ctPanel.add(ptTextField);
+
+		top.add(ctPanel);
+
+		// Key
+		JPanel keyPanel = new JPanel();
+		keyPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+
+		JLabel keyLabel = new JLabel("Key");
+		JTextField keyTextField = new JTextField(30);
+		keyTextField.setHorizontalAlignment(SwingConstants.RIGHT);
+		keyPanel.add(keyLabel);
+		keyPanel.add(keyTextField);
+
+		top.add(keyPanel);
+
+		/*
+		 * Mid Layout
+		 */
+		mid = new JPanel();
+		mid.setLayout(new FlowLayout());
+		Border midBD = BorderFactory.createLineBorder(Color.BLUE);
+		mid.setBorder(BorderFactory.createTitledBorder(midBD, "Thao tác"));
+
+		// Choose file
+		Button chooseFile = new Button("Chọn File");
+		mid.add(chooseFile);
+
+		// Create key
+		Button loadKey = new Button("Load khóa");
+		mid.add(loadKey);
+
+		// Decrypt
+		Button decrypt = new Button("Giải mã");
+		mid.add(decrypt);
+		
+		/*
+		 * Bot Layout
+		 */
+		bot = new JPanel();
+		bot.setLayout(new BorderLayout());
+		Border botBD = BorderFactory.createLineBorder(Color.gray);
+		bot.setBorder(botBD);
+		
+		JTextArea txtArea = new JTextArea(10, 800);
+		txtArea.append("Result");
+		bot.add(txtArea);
+		
+		JScrollPane scrollPane = new JScrollPane(txtArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS
+				, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		bot.add(scrollPane, BorderLayout.SOUTH);
+		
+		/*
+		 * Main DecryptPanel
+		 */
+		subPanel = new JPanel();
+		subPanel.setLayout(new BorderLayout());
+		subPanel.add(mid, BorderLayout.SOUTH);
+		subPanel.add(top, BorderLayout.CENTER);
+		add(subPanel);
+		
+		add(bot, BorderLayout.SOUTH);
+	}
+}
